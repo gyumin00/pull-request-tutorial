5d3c0872706b07517fa9004d306ddccc9c4486c1
gyumin <rbals2102@gmail.com>
2023-11-29 00:21:58 +0900

    fixed gyumin

--- BooleanTest.js
+++ BooleanTest.js
@@ -1,9 +1,11 @@
 var b = false;;
 
+
 if (b) {
-	console.log('This is true');
-} else {
 	console.log('This is false');
+} else {
+	console.log('This is true');
+
 }
 
-console.log('Fixed by John');
+console.log('Fixed by gyumin');
